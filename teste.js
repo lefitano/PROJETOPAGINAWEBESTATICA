9dedeb3da2bb1d8c76ff5535edb2b76117cb72f5
Leonardo Monteiro <leonardoms17@hotmail.com>
2025-11-12 19:02:10 -0300

    Adicionei o botão de Entrar em contato pelo Whatsapp, já entrando na url do número da empresa. Usando JavaScript

--- teste.js
+++ teste.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const botao = document.getElementById('btnContatoWPP');
+    const botaoWPP = document.getElementById('btnContatoWPP');
+    botaoWPP.addEventListener('click', () =>{
+        const numeroWPP = '5585999572062';
+        const mensagem = "Olá, Gostaria de saber mais sobre os produtos da Joab Kids";
+        const url = `https://wa.me/${numeroWPP}?text=${encodeURIComponent(mensagem)}`;
+        console.log(numeroWPP);
+        console.log(mensagem);
+        console.log(url);
+        window.open(url, '_blank');
+    })
 })
 document.addEventListener("DOMContentLoaded", (event) => {
     const botaoInsta = document.getElementById('btnContatoInsta');
